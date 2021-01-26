0b419a7fd8ddef7d15725990c9d02c230d5c5313
Nick <whiteshadow148@gmail.com>
2021-01-26 12:03:08 -0700

    Python script works

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 DdriveCleanup
+
+
+This is a script to clean up the temp files on users D drive.  The goal is this would be run with as a scheduled task and that it would cut down on calls.  We get lots of calls to clean up space on thin clients.
+
+The powershell script is the most efficeint way to do this.  But as an organization Sandy City has shut off the ablity to run scripts.  So the Python script is a second attempt at this.  
--- cleanup.py
+++ cleanup.py
@@ -1,10 +1,24 @@
 import os, getpass, os.path
+import shutil
 
 user = getpass.getuser()
 
 mypath = 'D:/Users/' + user + '/AppData/Local/Temp'
 
+for root, dirs, files in os.walk(mypath):
+    for file in files:
+        try:
+            os.remove(os.path.join(root, file))
+        except:
+            print("Failed on " + file)
 
 
-print(mypath)
-print(user)
+for root, dirs, files in os.walk(mypath, topdown=False):
+    for name in dirs:
+        try:
+            os.rmdir(os.path.join(root, name))
+        except:
+            print("Failed on " + name)
+    
+# print(mypath)
+# print(user)
